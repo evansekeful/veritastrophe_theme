f3139ab90d750211da67d6ff61081225ff801f72
Ellie Evans <ellie@solnimbus.com>
2023-04-12 16:50:38 -0700

    typography + section colors

--- template-parts/excerpt/excerpt.php
+++ template-parts/excerpt/excerpt.php
@@ -10,14 +10,19 @@
 ?>
 
 <p id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+	<header class="excerpt-header">
+		
+		<?php veritastrophe_post_thumbnail(); ?>	
+
 		<?php
 		if ( is_singular() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;?>
-		<?php veritastrophe_post_thumbnail(); ?>
+		
+		<!-- TODO: tags / author / date row -->
+
 	</header><!-- .entry-header -->
 
 	<div class="entry-excerpt">
